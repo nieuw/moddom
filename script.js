06f05b1a9c97f2a8c189cbc02192e7e41873394b
Robin Nieuwboer <rnieuwboer.contractor@libertyglobal.com>
2017-09-10 19:16:16 +0200

    removing all tags for re-adding

--- script.js
+++ script.js
@@ -38,140 +38,136 @@ function addRemoveTagButton(){
     if(range){
         //add or remove from selection;
         if(!range.collapsed){
-            if(isAllContentInsideTag(range, value))
-                console.log('remove (partial) tags')
-            else{
-                let startWrap = getWrapNode(range.startContainer, value)
-                    , endWrap = getWrapNode(range.endContainer, value)
-                    , start = startWrap || range.startContainer
-                    , end = endWrap || range.endContainer
-                    , startOffset = startWrap ? 0 : range.startOffset
-                    , endOffset = endWrap ? 1 : range.endOffset
+            let startWrap = getWrapNode(range.startContainer, value)
+                , endWrap = getWrapNode(range.endContainer, value)
+                , start = startWrap || range.startContainer
+                , end = endWrap || range.endContainer
+                , startOffset = startWrap ? 0 : range.startOffset
+                , endOffset = endWrap ? 1 : range.endOffset
+                , isRemove = isAllContentInsideTag(range, value);
 
-                if(join){
-                    if(startOffset === 0)
-                        start = getFarthestAdjacentwrappedNode(range.startContainer, value, 0)
-                    if(endOffset === end.length){
-                        end = getFarthestAdjacentwrappedNode(range.endContainer, value, 1);
-                        endOffset = end.length;
-                    }
-                }
-                
-                console.log(endOffset);
-                [
-                    [start,'wrap-start',startOffset]
-                    ,[end, 'wrap-end', endOffset]
-                    ,[range.startContainer, 'selection-start', range.startOffset]
-                    ,[range.endContainer, 'selection-end', range.endOffset]
-                ].forEach(function(v){
-                    //setMarker.apply(null, v)
-                })
-
-                getNodesFromTo(start, end).map(function(node){
-                    //if(isMatchedNode(node, value))
-                        //removeWrappingNode(node);
-                    if(node.hasChildNodes())
-                       console.dir(node.childNodes)
-                })
-                      
-
-                if(startWrap){
-                    removeWrappingNode(startWrap);
+            if(join){
+                if(startOffset === 0)
+                    start = getFarthestAdjacentwrappedNode(range.startContainer, value, 0)
+                if(endOffset === end.length){
+                    end = getFarthestAdjacentwrappedNode(range.endContainer, value, 1);
+                    endOffset = end.length;
                 }
             }
-        }else
-            console.log(isNodeWrapped(range.startContainer, value) ? 'break tag' : 'add tag')
 
-    }
-}
-function setMarker(node, markerID, offset){
-    let marker = '<span id="'+markerID+'"></span>';
-    console.log(node)
-    if(node.nodeType === 3){
-        let parent = node.parentNode
-            ,dummy = document.createElement('div')
-            ,frag = document.createDocumentFragment()
-            ,child
+            const markers = {
+                'wrapStart': getSetMarker(end, 'wrap-end', endOffset),
+                'selectionStart': getSetMarker(range.endContainer, 'selection-end', range.endOffset),
+                'selectionEnd': getSetMarker(range.startContainer, 'selection-start', range.startOffset),
+                'wrapEnd': getSetMarker(start,'wrap-start',startOffset)
+            }
 
-        dummy.innerHTML = node.nodeValue.slice(0, offset)+marker+node.nodeValue.slice(offset)
-        while(child = dummy.firstChild)
-            frag.appendChild(child)
-        
-        parent.replaceChild(frag, node)
-    }else{
-        console.log(offset)
-        node.innerHTML = marker + node.innerHTML;
-    }
-    
-}
+            removeAllTagsOfTypeBetween(value, markers.wrapStart, markers.wrapEnd);
+            
+            if(isRemove){ 
+                wrapContenWithTag(markers.wrapStart, markers.selectionStart)
+                wrapContenWithTag(markers.selectionEnd, markers.wrapEnd) 
+            }else 
+                wrapContenWithTag(markers.wrapStart, markers.wrapEnd) 
+        }else 
+            console.log(isNodeWrapped(range.startContainer, value) ? 'break tag' : 'add tag') 
+    } 
+} 
 
-function getSelectionRange(){
-    if(selection.rangeCount){
-        let range = selection.getRangeAt(0) 
+function removeAllTagsOfTypeBetween(type, start, end){ 
+    getNodesFromTo(start, end).forEach(function(node){
+        if(node.nodeType === 1){ 
+            node.querySelectorAll(type).forEach(function(childNode){ 
+                removeWrappingNode(childNode) 
+            }) 
+            if(isMatchedNode(node, type)) 
+                removeWrappingNode(node); 
+        } 
+    }) 
+} 
 
-        if(wysiwyg.contains(range.commonAncestorContainer))
-            return range;
-    }
-    return null;
-}
-function getNextChildlessNode(node, direction){
-    const getSibling = ['previousSibling','nextSibling']
-        , getChild = ['lastChild', 'firstChild']
-        
-    let nextChildLess = false;
-
-    while(!node[getSibling[direction]]){
-        node = node.parentNode;
-        if(!node || node === wysiwyg){
-            return false;
-        }
-    }
-    nextChildLess = node[getSibling[direction]]
-    if(nextChildLess){
-        while(nextChildLess.hasChildNodes()){
-            nextChildLess = nextChildLess[getChild[direction]];
-        }
-    }
-    return nextChildLess;
-}
-
-function getFarthestAdjacentwrappedNode(node, wrap, direction){
-    let foundNode = false
-        , newFoundNode
-
-    while(node = getNextChildlessNode(node, direction)){
-        newFoundNode = getWrapNode(node, wrap)
-        if(!newFoundNode)
-            break;
+function getSetMarker(node, markerName, offset){ 
+    const marker = document.createElement('span') 
+    marker.className = markerName; 
+    if(node.nodeType === 3) 
+        node.parentNode.insertBefore(marker, node.splitText(offset));
+    else
+        if(offset === 0) 
+            node.parentNode.insertBefore(marker, node) 
         else
-            foundNode = newFoundNode
-            node = foundNode;
-    }
-    return foundNode;
-}
-function getNodesFromTo(start, end, commonAncestorContainer){
-    return [start, end].concat(getNodesInBetween(start, end, commonAncestorContainer));
-}
+            if(node === node.parentNode.lastChild) 
+                node.parentNode.appendChild(marker)
+            else 
+                node.parentNode.insertBefore(marker, node.nextSibling) 
 
-function isAllContentInsideTag(range, tag){
-    return getNodesFromTo(range.startContainer, range.endContainer, range.commonAncestorContainer).every(function(v){
-        console.log(v, isNodeWrapped(v, tag), isContentWrapped(v, tag))
-        return isNodeWrapped(v, tag) || isContentWrapped(v, tag)
-    })
-}
+    return marker; 
+} 
 
-function getWrapNode(node, wrap, rootNode){
-    rootNode = rootNode || wysiwyg;
-    let ancestors = getTreeForNode(node, rootNode)
-        , found = false;
+function getSelectionRange(){ 
+    if(selection.rangeCount){ 
+        let range = selection.getRangeAt(0) 
+        if(wysiwyg.contains(range.commonAncestorContainer)) 
+            return range; 
+    } 
+    return null; 
+} 
 
-    for(let i = 0, l = ancestors.length; i<l; i++)
-        if(ancestors[i].localName.toLowerCase() === wrap){
-            found = ancestors[i];
-            break;
-        }
-        
-    return found;
+function getNextChildlessNode(node, direction){ 
+    const getSibling = ['previousSibling','nextSibling'] 
+        , getChild = ['lastChild', 'firstChild'] 
+    let nextChildLess = false; 
+
+    while(!node[getSibling[direction]]){ 
+        node = node.parentNode; 
+        if(!node || node === wysiwyg){ 
+            return false; 
+        } 
+    } 
+
+    nextChildLess = node[getSibling[direction]] 
+    if(nextChildLess){ 
+        while(nextChildLess.hasChildNodes()){ 
+            nextChildLess = nextChildLess[getChild[direction]]; 
+        } 
+    } 
+    return nextChildLess; 
+} 
+
+function getFarthestAdjacentwrappedNode(node, wrap, direction){ 
+    let foundNode = false 
+        , newFoundNode 
+    
+    while(node = getNextChildlessNode(node, direction)){ 
+        newFoundNode = getWrapNode(node, wrap) 
+        if(!newFoundNode) break; 
+        else 
+            foundNode = newFoundNode 
+            node = foundNode; 
+    } 
+    return foundNode; 
+} 
+
+function getNodesFromTo(start, end, commonAncestorContainer){ 
+    return [start, end].concat(getNodesInBetween(start, end, commonAncestorContainer)); 
+} 
+
+function isAllContentInsideTag(range, tag){ 
+    return getNodesFromTo(range.startContainer, range.endContainer, range.commonAncestorContainer).every(function(v){ 
+        return isNodeWrapped(v, tag) || isContentWrapped(v, tag) 
+    }) 
+} 
+
+function getWrapNode(node, wrap, rootNode){ 
+    rootNode = rootNode || wysiwyg; 
+    let ancestors = getTreeForNode(node, rootNode) 
+        , found = false; 
+
+    for(let i = 0, l = ancestors.length; i<l; i++) 
+        if(ancestors[i].localName.toLowerCase() === wrap){ 
+            found = ancestors[i]; 
+            break; 
+        } 
+        return found;
 }
 
 function isNodeWrapped(node, wrap, rootNode){
